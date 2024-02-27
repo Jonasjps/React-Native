7cda8f29ae5545e1453b94f2e3c73897c1cd753c
Jonas Pessoa <Jonaspsjw@hotmail.com>
2024-02-26 22:43:06 -0300

    Atualizando repos react-native

---
+++ meuApp2/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Olá Mundo!!!</Text>
+        <Text>Meu Primeiro app</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
